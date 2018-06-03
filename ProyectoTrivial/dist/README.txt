9c2df5a4acdfd332433544ef8f7ca84d9f49698d
Javier <Javier@DESKTOP-5JHHNVL>
2018-06-03 20:52:32 +0200

    Pequeños cambios. Comentarios JavaDoc de Pregunta.

--- ProyectoTrivial/dist/README.txt
+++
@@ -1,1 +0,0 @@
-Última versión del proyecto. Para que funcione es obligatorio descargarse el jar Y la carpeta lib. Disfruta! :D
--- ProyectoTrivial/src/trivial/Pregunta.java
+++ ProyectoTrivial/src/trivial/Pregunta.java
@@ -5,42 +5,63 @@ package trivial;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
- *
- * @author javpin
+ * Clase que representa una pregunta trivia y sus posibles respuestas.
+ * @author Javier
  */
 public class Pregunta {
 
+    /**
+     * Texto de la pregunta
+     */
     public String pregunta = "";
+    /**
+     * Vector son el texto de las respuestas
+     */
     public String respuestas[] = {"", "", "", ""};
-    //public static String preguntas[] = {"", "", "", ""};
 
-    //public String[] leepreguntas() {
-    //    return preguntas;
-    //}
-
+    /**
+     * Devuelve la pregunta
+     * @return La pregunta
+     */
     public String getPregunta() {
         return pregunta;
     }
 
+    /**
+     * Asigna la pregunta
+     * @param pregunta La pregunta
+     */
     public void setPregunta(String pregunta) {
         this.pregunta = pregunta;
     }
 
+    /**
+     * Devuelve el vector de respuestas
+     * @return Las respuestas
+     */
     public String[] getRespuestas() {
         return respuestas;
     }
 
+    /**
+     * Establece el vector de respuestas
+     * @param respuestas Las respuestas
+     */
     public void setRespuestas(String[] respuestas) {
         this.respuestas = respuestas;
     }
 
+    /**
+     * Imprime por salida estandar toda la información de esta clase (preguntas y respuestas)
+     */
     public void imprimePregunta() {
         System.out.println("P:" + pregunta);
-        System.out.println("R1:" + respuestas[2]);
-        System.out.println("R2:" + respuestas[3]);
-        System.out.println("R3:" + respuestas[0]);
-        System.out.println("R4:" + respuestas[1]);
+        System.out.println("R1:" + respuestas[0]);
+        System.out.println("R2:" + respuestas[1]);
+        System.out.println("R3:" + respuestas[2]);
+        System.out.println("R4:" + respuestas[3]);
     }
 
 }
